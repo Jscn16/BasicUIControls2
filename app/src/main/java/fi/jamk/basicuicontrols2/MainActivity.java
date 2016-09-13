97a5ed32343934cc70a8f2fe213cf9b467b5e061
Jscn16 <janis_c@gmx.de>
2016-09-13 16:42:52 +0300

    implented toast

--- app/src/main/java/fi/jamk/basicuicontrols2/MainActivity.java
+++ app/src/main/java/fi/jamk/basicuicontrols2/MainActivity.java
@@ -2,8 +2,11 @@ package fi.jamk.basicuicontrols2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,17 +21,23 @@ public class MainActivity extends AppCompatActivity {
         actv.setAdapter(aa);
     }
 
-    /* TO IMPLEMENT, SHOW TOAST
+    /* TO IMPLEMENT, SHOW TOAST*/
     public void selectButtonClicked(View view) {
         //find radiogroup
-        RadioGroup rg = (RadioGroup) findViewById(R.id.radioGroup);
-        int id = rg.getCheckedRadioButtonId();
+        //RadioGroup rg = (RadioGroup) findViewById(R.id.radioGroup);
+        //int id = rg.getCheckedRadioButtonId();
         //find button
-        RadioButton rb = (RadioButton) findViewById(id);
-        //get radiobutton text
-        String text = (String) rb.getText();
+        //RadioButton rb = (RadioButton) findViewById(id);
+        //get editable text
+        //String text = (String) rb.getText();
         //toast message to screen
-        Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
-    } */
+        EditText etlogin = (EditText) findViewById(R.id.editTextLogin);
+        String textlogin = (String) etlogin.getText().toString();
+
+        EditText etpassword = (EditText) findViewById(R.id.editTextPassword);
+        String textpassword = (String) etpassword.getText().toString();
+
+        Toast.makeText(getApplicationContext(), textlogin + " " + textpassword, Toast.LENGTH_SHORT).show();
+    }
 
 }
